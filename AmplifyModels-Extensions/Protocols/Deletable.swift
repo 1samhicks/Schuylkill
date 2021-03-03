5fed754de35f0eb70ac4fce992a62bc721973851
Sammy Hicks <1samhicks@gmail.com>
2021-03-03 02:26:00 -0500

    We need this so we can use type poymorphism and allow the compiler to know anyone realizing Model has a deleted flag which we can use for 'soft deletes.'

---
+++ AmplifyModels-Extensions/Protocols/Deletable.swift
@@ -0,0 +1,30 @@
+//
+//  Deletable.swift
+//  Schuylkill-App
+//
+//  Created by Sam Hicks on 3/2/21.
+//
+
+import Foundation
+
+protocol Deletable {
+    var deleted: Bool? { get set }
+}
+
+extension ExerciseMachine : Deletable {}
+
+extension ExerciseRep : Deletable {}
+
+extension ExerciseSet : Deletable {}
+
+extension FitnessCenter : Deletable {}
+
+extension GymWorkout : Deletable {}
+
+extension Location : Deletable {}
+
+extension Muscle : Deletable {}
+
+extension MotionAttitude : Deletable {}
+
+extension AccelerometerReading : Deletable {}
