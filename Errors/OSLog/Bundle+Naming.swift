282cb18166fa6d55d87da16faed8811b6426b53f
Sammy Hicks <1samhicks@gmail.com>
2021-03-03 02:36:13 -0500

    Logging Infrastructure

---
+++ Errors/OSLog/Bundle+Naming.swift
@@ -0,0 +1,15 @@
+//
+//  Bundle+Naming.swift
+//  Schuylkill-App
+//
+//  Created by Sam Hicks on 3/2/21.
+//
+
+import Foundation
+
+extension Bundle {
+    enum Naming : String {
+        case dependencyInjection = "dependency_injection_registration"
+        
+    }
+}
---
+++ Errors/OSLog/Logger.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Schuylkill-App
+//
+//  Created by Sam Hicks on 3/2/21.
+//
+
+import Foundation
+import OSLog
+import Resolver
+
+extension OSLog {
+    private static var logger = Logger()
+    private static var subsystem = Bundle.main.bundleIdentifier!
+    
+    static let diRegistration = OSLog(subsystem: subsystem, category: "dependency_injection_registration")
+    
+    static func registerService(resolved: ResolverRegistrant.Type,name : Resolver.Name, key : Int, containerName: String) {
+        let message = "Registering service: \(resolved.self) named: \(name) with key: \(key) in container: \(containerName)"
+        print(message)
+    }
+}
