2450cbfced3a60befb46abf870636a272ab906d9
Sammy Hicks <1samhicks@gmail.com>
2021-03-06 14:46:32 -0500

    More functionality for the upload/download operations on the S3 Service

--- Services/Amplify Services/AmplifyS3StorageService.swift
+++ Services/Amplify Services/AmplifyS3StorageService.swift
@@ -18,18 +18,47 @@ class AmplifyS3StorageService : RuntimeService {
     required public init() {
         
     }
+    
+    func uploadData(key : String, dataString : String) {
+        let data = dataString.data(using: .utf8)!
+        let storageOperation = Amplify.Storage.uploadData(key: key, data: data)
+        progressSink = storageOperation.progressPublisher.sink { progress in print("Progress: \(progress)") }
+        resultSink = storageOperation.resultPublisher.sink {
+            if case let .failure(storageError) = $0 {
+                print("Failed: \(storageError.errorDescription). \(storageError.recoverySuggestion)")
+            }
+        }
+        receiveValue: { data in
+            print("Completed: \(data)")
+        }
+    }
+    
+    func uploadData(key : String, nameOfFile : String) {
+        let filename = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent(nameOfFile)
 
-    func uploadData(name: String, image : UIImage) -> StorageUploadDataOperation? {
-        guard let dataString = image.pngData() else { return nil }
+        let storageOperation = Amplify.Storage.uploadFile(key: key, local: filename)
+        progressSink = storageOperation.progressPublisher.sink { progress in print("Progress: \(progress)") }
+        resultSink = storageOperation.resultPublisher.sink {
+            if case let .failure(storageError) = $0 {
+                print("Failed: \(storageError.errorDescription). \(storageError.recoverySuggestion)")
+            }
+        }
+        receiveValue: { data in
+            print("Completed: \(data)")
+        }
+    }
+
+    func uploadData(name: String, image : UIImage)  {
+        guard let dataString = image.pngData() else { return }
         let uploadOperation = Amplify.Storage.uploadData(key: name, data: dataString)
-        return uploadOperation
         
-        /***
-         progressSink = storageOperation
+        
+         progressSink = uploadOperation
              .progressPublisher
              .sink { progress in print("Progress: \(progress)") }
 
-         resultSink = storageOperation
+         resultSink = uploadOperation
              .resultPublisher
              .sink {
                  if case let .failure(storageError) = $0 {
@@ -39,6 +68,36 @@ class AmplifyS3StorageService : RuntimeService {
              receiveValue: { data in
                  print("Completed: \(data)")
              }
-         */
+    }
+    
+    func downloadToFile(named fileName : String, fromKey key : String) {
+        let downloadToFileName = FileManager.default.urls(for: .documentDirectory,
+                                                          in: .userDomainMask)[0]
+            .appendingPathComponent(fileName)
+        let storageOperation = Amplify.Storage.downloadFile(key: key, local: downloadToFileName)
+        progressSink = storageOperation.progressPublisher.sink { progress in print("Progress: \(progress)") }
+        resultSink = storageOperation.resultPublisher.sink {
+            if case let .failure(storageError) = $0 {
+                print("Failed: \(storageError.errorDescription). \(storageError.recoverySuggestion)")
+            }
+        }
+        receiveValue: {
+            print("Completed")
+        }
+    }
+    
+    func download(key : String) -> Data? {
+        var returnData : Data?
+        let storageOperation = Amplify.Storage.downloadData(key: key)
+        progressSink = storageOperation.progressPublisher.sink { progress in print("Progress: \(progress)") }
+        resultSink = storageOperation.resultPublisher.sink {
+            if case let .failure(storageError) = $0 {
+                print("Failed: \(storageError.errorDescription). \(storageError.recoverySuggestion)")
+            }
+        }
+        receiveValue: { data in
+            returnData = data
+        }
+        return returnData
     }
 }
