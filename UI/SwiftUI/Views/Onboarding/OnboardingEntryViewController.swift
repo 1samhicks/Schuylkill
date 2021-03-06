6385e2fba4d4ab4a41d625d36168578d30cdd0ae
Sammy Hicks <1samhicks@gmail.com>
2021-03-06 13:28:30 -0500

    Two new classes: one for entry of onboarding, the other is a wrapper for Image assets.

---
+++ UI/SwiftUI/Views/Onboarding/OnboardingEntryViewController.swift
@@ -0,0 +1,73 @@
+//
+//  OnboardingEntryViewController.swift
+//  Schuylkill-App
+//
+//  Created by Sam Hicks on 3/6/21.
+//
+
+import Foundation
+import UIKit
+import PaperOnboarding
+
+public class OnboardingEntryViewController : UIViewController, PaperOnboardingDataSource {
+    public func onboardingItemsCount() -> Int {
+        return 3
+    }
+    
+    public func onboardingItem(at index: Int) -> OnboardingItemInfo {
+
+       return [
+         OnboardingItemInfo(informationImage: Asset.key.image,
+                                       title: "title",
+                                 description: "description",
+                                    pageIcon: Asset.key.image,
+                                       color: UIColor.red,
+                                  titleColor: UIColor.green,
+                            descriptionColor: UIColor.gray,
+                            titleFont: UIFont.italicSystemFont(ofSize: 14),
+                             descriptionFont:UIFont.italicSystemFont(ofSize: 10) ),
+
+         OnboardingItemInfo(informationImage: Asset.key.image,
+                                        title: "title",
+                                  description: "description",
+                                     pageIcon: Asset.key.image,
+                                        color: UIColor.red,
+                                   titleColor: UIColor.green,
+                             descriptionColor: UIColor.gray,
+                                    titleFont: UIFont.italicSystemFont(ofSize: 14),
+                              descriptionFont: UIFont.italicSystemFont(ofSize: 10)),
+
+        OnboardingItemInfo(informationImage: Asset.key.image,
+                                     title: "title",
+                               description: "description",
+                                  pageIcon: Asset.key.image,
+                                     color: UIColor.red,
+                                titleColor: UIColor.green,
+                          descriptionColor: UIColor.gray,
+                                 titleFont: UIFont.italicSystemFont(ofSize: 14),
+                           descriptionFont: UIFont.italicSystemFont(ofSize: 10))
+         ][index]
+     }
+    
+    public override func viewDidLoad() {
+      super.viewDidLoad()
+
+      let onboarding = PaperOnboarding()
+      onboarding.dataSource = self
+      onboarding.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(onboarding)
+
+      // add constraints
+        for attribute: NSLayoutConstraint.Attribute in [.left, .right, .top, .bottom] {
+        let constraint = NSLayoutConstraint(item: onboarding,
+                                            attribute: attribute,
+                                            relatedBy: .equal,
+                                            toItem: view,
+                                            attribute: attribute,
+                                            multiplier: 1,
+                                            constant: 0)
+        view.addConstraint(constraint)
+      }
+    }
+
+}
