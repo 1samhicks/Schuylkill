94dc6bd1f93ff5f609b46b9f5b07b694d64b234d
Sammy Hicks <1samhicks@gmail.com>
2021-03-08 17:58:05 -0500

    We need this but will revisit.

---
+++ Errors/Extensions/PluginError.swift
@@ -0,0 +1,18 @@
+//
+//  PluginError.swift
+//  Schuylkill-App
+//
+//  Created by Sam Hicks on 3/7/21.
+//
+
+import Foundation
+import Amplify
+import AmplifyPlugins
+extension PluginError : ErrorHandling {}
+
+extension ConfigurationError : ErrorHandling {}
+
+extension LoggingError : ErrorHandling {}
+
+extension AnalyticsError : ErrorHandling {}
+
