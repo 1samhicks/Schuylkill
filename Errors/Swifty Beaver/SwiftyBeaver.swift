2a041063f2888e337aa85aaa9941130114f45bec
Sammy Hicks <1samhicks@gmail.com>
2021-03-13 10:32:07 -0500

    Write the Log to a file as well using the Disk subsystem

--- Errors/Swifty Beaver/SwiftyBeaver.swift
+++ Errors/Swifty Beaver/SwiftyBeaver.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftyBeaver
+import Disk
 
 extension SwiftyBeaver {
     typealias secrets = SensitiveConstants.SwiftyBeaver
@@ -14,17 +15,19 @@ extension SwiftyBeaver {
     public static func activate() {
         // add log destinations. at least one is needed!
         let console = ConsoleDestination()  // log to Xcode Console
-        let file = FileDestination()  // log to default swiftybeaver.log file
         //let cloud = SBPlatformDestination(appID: "foo", appSecret: "bar", encryptionKey: "123") // to cloud
         console.format = "$DHH:mm:ss$d $L $M"
         
         log.addDestination(console)
-        log.addDestination(file)
+        log.addDestination(BaseDestination())
         log.addDestination(SBPlatformDestination(appID: secrets.APP_ID, appSecret: secrets.APP_SECRET, encryptionKey: secrets.ENCRYPTION_KEY))
     }
     
+    public static func exceptionThrown(error : Error) {
+        log.error(error)
+    }
+    
     public static func exceptionThrown(args : [String : String]) {
-        
         log.error(args)
     }
 }
