f2cee1757cb18f59495421df0ea1baf030033512
Sammy Hicks <1samhicks@gmail.com>
2021-03-13 11:25:39 -0500

    Break out this new ServiceLifecycle protocol.

--- Services/Protocols/DeviceService.swift
+++ Services/Protocols/DeviceService.swift
@@ -11,56 +11,21 @@ import Combine
 #if !os(watchOS)
 import OSLog
 #endif
-protocol DeviceService : RuntimeService {
-    typealias DeviceServiceStateTransition = (() -> ())?
-    var state : ServiceState? { get set } //TODO: This shouldn't be visible to the outside world
-    var dispatchSemaphore : DispatchSemaphore { get set }
+
+protocol DeviceService: RuntimeService, ServiceLifecycle {
+    typealias DeviceServiceStateTransition = (() -> Void)?
+    var state: ServiceState? { get set } // TODO: This shouldn't be visible to the outside world
+    var lock: RecursiveLock { get set }
     var resultSink: AnyCancellable { get set }
-    func startService()
-    func pauseService()
-    func unpauseService()
-    func endService()
-    var motionManager : CMMotionManager { get }
-    var fifoOperationQueue : OperationQueue { get }
+    var motionManager: CMMotionManager { get }
+    var fifoOperationQueue: OperationQueue { get }
     mutating func setNewServiceState(newState: ServiceState) -> DeviceServiceStateTransition
 }
 
-extension DeviceService {
-    
-}
-
-extension DeviceService {
-    
-    public var motionManager: CMMotionManager {
-        CMMotionManager.shared
-    }
-    public var fifoOperationQueue : OperationQueue {
-        OperationQueue.shared
-    }
-    /*public mutating func setNewServiceState(newState: ServiceState) -> DeviceServiceStateTransition {
-        switch newState {
-            case .notStarted:
-                return { ([weak self]) in self.state = newState }
-            case .canceled:
-                fallthrough
-            case .stopped:
-                fallthrough
-            case .finished:
-                return { self.endService()}
-            case .running:
-                return { self.startService()}
-            case .paused:
-                return { self.pauseService() }
-    }*/
-}
-
-
-extension CMMotionManager  {
+extension CMMotionManager {
     static let shared = CMMotionManager()
 }
 
 extension OperationQueue {
     static let shared = OperationQueue()
 }
-
-
