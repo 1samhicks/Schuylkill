c8e7de08107400cccaad09a594c2e49cbcec1dc2
Sammy Hicks <1samhicks@gmail.com>
2021-03-13 00:31:12 -0500

    Remove the source code markers

--- Errors/ApplicationRuntimeError.swift
+++ Errors/ApplicationRuntimeError.swift
@@ -9,25 +9,24 @@ import Foundation
 
 public enum ApplicationRuntimeError : ApplicationError, ErrorHandling {
     
-<<<<<<< Updated upstream
-    case InconsistentState(ErrorDescription,RecoverySuggestion = RecoverySuggestion.none)
-    case WatchConfigurationIssue(ErrorDescription, RecoverySuggestion = RecoverySuggestion.none)
-    case UnidentifiedError(ErrorDescription, RecoverySuggestion = RecoverySuggestion.none)
-    
-    var error: Error? {
-=======
     case InconsistentState(ErrorDescription,RecoverySuggestion = RecoverySuggestion.empty)
     case WatchConfigurationIssue(ErrorDescription, RecoverySuggestion = RecoverySuggestion.empty)
     case UnidentifiedError(ErrorDescription, RecoverySuggestion = RecoverySuggestion.empty)
     
-    var underlyingError: Error? {
->>>>>>> Stashed changes
+    public init(errorDescription: ErrorDescription, recoverySuggestion: RecoverySuggestion, error: Error) {
+        //self.init(errorDescription: errorDescription,recoverySuggestion: recoverySuggestion,error: error)
+        self = .UnidentifiedError("This was set in the ApplicationRuntimeError", "?")
+    }
+    
+    public var underlyingError: Error? {
         get {
             return nil
         }
     }
     
-    var errorDescription: ErrorDescription {
+    
+    
+    public var errorDescription: ErrorDescription {
         switch self {
             case .InconsistentState(let description,_):
                 fallthrough
@@ -38,7 +37,7 @@ public enum ApplicationRuntimeError : ApplicationError, ErrorHandling {
        }
     }
     
-    var recoverySuggestion : RecoverySuggestion {
+    public var recoverySuggestion : RecoverySuggestion {
         switch self {
             case .InconsistentState(_,let suggestion):
                 fallthrough
