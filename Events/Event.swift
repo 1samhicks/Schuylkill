c875acd3fe7a3c3c27515b9124ad6362bddfa4ea
Sammy Hicks <1samhicks@gmail.com>
2021-03-20 18:01:20 -0400

    Amplify again.

--- Events/Event.swift
+++ Events/Event.swift
@@ -6,6 +6,31 @@
 //
 
 import Foundation
+import CoreLocation
+import CoreMotion
 
 public protocol Event {
 }
+
+public enum DeviceEvent: Event {
+    case logItemEvent(CMLogItem)
+    case locationEvent(CLLocation)
+    case headingEvent(CLHeading)
+    case enteredRegion(CLRegion)
+    case exitedRegion(CLRegion)
+    case pedometerEvent(CMPedometerEvent)
+}
+
+#if !os(watchOS)
+import Amplify
+
+public enum AmplifyMutationEvent: Event {
+    case mutationEvent(MutationEvent)
+}
+
+public enum AuthenticationEvent: Event {
+    case started
+    case mutationEvent(MutationEvent)
+}
+#endif
+
